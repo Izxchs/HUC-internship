c34bfc64da2a49d6d4a91df5145d093a9d891b3b
Izxchs <ihsanrafeeq007@gmail.com>
2026-02-16 17:07:11 +0530

    Initial clean commit

---
+++ Simple ToDo APP/script.js
@@ -0,0 +1,25 @@
+function addTask() {
+    const input = document.getElementById("taskInput");
+    const taskText = input.value;
+
+    if (taskText === "") {
+        alert("please enter a task");
+        return;
+    }
+
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+
+    deleteBtn.onclick = function () {
+        li.remove();
+    };
+
+    li.appendChild(deleteBtn);
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
